1e0b80595f34519748eaf1b4c923f84891b3eca9
NielsTRS <trsniels@gmail.com>
2024-09-19 09:49:26 +0200

    Ajout code C

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello World 2!");
+    let mut p = Point { x: 1, y: 2 };
+    println!("Point : {:?}", p);
+    println!("Point : x: {}, y: {}", p.x + 1, p.y);
+    println!("Point : {:?}", p);
+    p.x += 1;
+    println!("Point : {:?}", p);
 }
